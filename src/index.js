2dd52475919f82bfdf3b84f676d2d9e0322393be
Sora <sora@mbp-de-sora.home>
2024-02-07 22:48:48 +0100

    creation de l'environnement du projet et du fichier index

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import "dotenv/config";
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log(`[DATABSE] MongoDBSoBook is connected`);
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.json("Welcome to SoBook"); 
+});
+
+
+app.listen(port, () =>
+console.log(`[SERVER] listening at http://localhost:${port}`)
+);
