336f794d3eb04f820cc2c675e36a7372ad1eed6b
Sora <sora@mbp-de-sora.home>
2024-03-07 14:29:43 +0100

    todo modified & bookModel.js

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,12 +1,19 @@
 import { Schema, model } from "mongoose";
 
 
-const bookSchema = new Schema({
+const bookSchema = new Schema(
+    {
     title: { type: "string", required: true },
     image: { type: "string", required: true },
     description: { type: "string", required: true },
-    auteur: { type: "string", required: true },
+    author: { type: "string", required: true },
     synopsis: { type: "string", required: true },
-})
+    publishYear: { type: "number", required: true },
+    page: { type: "number", required: true }
+    },
+    {
+        timestamps: true,
+    }
+)
 
 export const Book = model("Book", bookSchema);
